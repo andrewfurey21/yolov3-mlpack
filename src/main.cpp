1d9882b9f0f44f75bd282f892c11d3a542238b63
Andrew Furey <andrewfurey2003@gmail.com>
2024-06-15 12:39:28 +0100

    updated CMakeLists.txt to fix linking problem with armadilloP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <mlpack.hpp>
+#include <armadillo>
+#include "../models/models/alexnet/alexnet.hpp"
 #include <iostream>
 
 int main(void) {
-	std::cout <<"Hello world" << std::endl;
+	mlpack::models::AlexNet net = mlpack::models::AlexNet();	
+	std::cout << "Hello world\n";
 	return 0;
 }
