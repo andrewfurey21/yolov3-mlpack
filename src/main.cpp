ec2b7c983e3e3d9d5c6d2e9ba5f9e0fb779bc32c
Andrew Furey <andrewfurey2003@gmail.com>
2024-07-06 22:39:55 +0100

    fill, embed, resize

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
-#include <cstdint>
 #include <mlpack.hpp>
 #include <armadillo>
-#include <stdexcept>
-#include <stdio.h>
-
-#include "../models/models/yolov3_tiny/yolov3_tiny.hpp"
-
-
-#define INPUT "input.jpg"
-#define OUTPUT "output.jpg"
-#define WEIGHTS "yolov3-tiny.weights"
-
-using namespace mlpack::models;
 
 struct detection {
   double x, y, w, h;
@@ -19,142 +7,113 @@ struct detection {
   double objectness;
 };
 
-class Image {
-public:
-  Image() :
-    width(0),
-    height(0),
-    channels(0)
-  {}
+void load(const std::string& file, arma::Mat<double>& data, mlpack::data::ImageInfo& info) {
+  mlpack::data::Load(file, data, info, true);
+  data /= 255.0f;
+}
 
-  Image(size_t width, size_t height, size_t channels) :
-  width(width),
-  height(height),
-  channels(channels)
-  {
-    data = std::vector<double>(width * height * channels);
-  }
+void save(const std::string& file, arma::Mat<double> data, mlpack::data::ImageInfo& info) {
+  data *= 255;
+  mlpack::data::Save(file, data, info, true);
+}
 
-  void load(const char* fileName) {
-    int width, height, channels;
-    uint8_t* data = stbi_load(fileName, &width, &height, &channels, 3);
-    if (!data) throw std::runtime_error("Could not load image\n");
+void fill(arma::Mat<double>& data, double value) {
+  value = std::clamp<double>(value, 0.0f, 1.0f);
+  data.fill(value);
+}
 
-    this->channels = 3;
-    this->width = width;
-    this->height = height;
+arma::Mat<double> resize(const arma::Mat<double>& data, 
+                         mlpack::data::ImageInfo& info, 
+                         mlpack::data::ImageInfo& outputInfo,
+                         double widthScale, 
+                         double heightScale) {
+  assert(widthScale > 0 && heightScale > 0);
+  size_t newWidth = std::floor(info.Width() * widthScale);
+  size_t newHeight = std::floor(info.Height() * heightScale);
 
-    for (int c = 0; c < this->channels; ++c){
-        for (int j = 0; j < this->height; ++j){
-            for (int i = 0; i < this->width; ++i){
-                int dest = i + this->width*j + this->width*this->height*c;
-                int source = c + c*i + c*this->width*j;
-                this->data[dest] = (double)data[source]/255.0f;
-            }
-        }
-    }
-    stbi_image_free(data);
-  }
+  assert(data.n_rows == info.Width() * info.Height() * 3 && data.n_cols == 1);
+  assert(newWidth > 1);
+  assert(newHeight > 1);
+  arma::Mat<double> resized(newWidth * newHeight * 3, 1);
+  outputInfo = mlpack::data::ImageInfo(newWidth, newHeight, 3);
 
-  void save(const char* fileName) {
-    uint8_t* data = (uint8_t*)calloc(this->width*this->height*this->channels, sizeof(uint8_t)); 
-    for (int c = 0; c < this->channels; ++c) {
-        for (int i = 0; i < this->width*this->height; ++i) {
-            data[i*this->channels+c] = (uint8_t)(255*this->data[i + c*this->width*this->height]);
-        }
-    }
-    int success = stbi_write_jpg(fileName, this->width, this->height, this->channels, data, 80);
-    if (!success) {
-	throw std::runtime_error("Could not write to image\n");
-    }
-    free(data);
-  }
+  double xRatio = (double)(info.Width() - 1) / (newWidth - 1);
+  double yRatio = (double)(info.Height() - 1) / (newHeight - 1);
 
-  void embed(Image& source, Image& dest, size_t dx, size_t dy) {
-    for (size_t c = 0; c < source.channels; c++) {
-      for (size_t y = 0; y < source.height; y++) {
-        for (size_t x = 0; x < source.width; x++) {
-          double value = source.getPixel(x, y, c);
-          dest.setPixel(x+dx, y+dy, c, value);
-        }
+  for (size_t channel = 0; channel < info.Channels(); channel++) {
+    for (size_t i = 0; i < newWidth; i++) {
+      for (size_t j = 0; j < newHeight; j++) {
+        size_t xLow = std::floor(xRatio * i);
+        size_t yLow = std::floor(yRatio * j);
+
+        size_t xHigh = std::ceil(xRatio * i);
+        size_t yHigh = std::ceil(yRatio * j);
+
+        size_t xWeight = (xRatio * i) - xLow;
+        size_t yWeight = (yRatio * j) - yLow;
+
+        //double a = data.at(yLow * info.Width() + xLow);
+        double a = data.at(yLow * info.Width() * info.Channels() + xLow * info.Channels() + channel);
+        //double b = data.at(yLow * info.Width() + xHigh);
+        double b = data.at(yLow * info.Width() * info.Channels() + xHigh * info.Channels() + channel);
+        //double c = data.at(yHigh * info.Width() + xLow);
+        double c = data.at(yHigh * info.Width() * info.Channels() + xLow * info.Channels() + channel);
+        //double d = data.at(yHigh * info.Width() + xHigh);
+        double d = data.at(yHigh * info.Width() * info.Channels() + xHigh * info.Channels() + channel);
+
+        double value = 
+                a * (1 - xWeight) * (1 - yWeight) +
+                b * xWeight * (1 - yWeight) +
+                c * yWeight * (1 - xWeight) +
+                d * xWeight * yWeight;
+
+        resized.at(j * newWidth * info.Channels() + i * info.Channels() + channel) = value;
       }
     }
   }
+  return resized;
+}
 
-  void resize(Image& source, Image& dest, size_t newWidth, size_t newHeight) {
-    dest.data = std::vector<double>(newWidth * newHeight * source.channels);
-    //TODO: interpolate across x and y
-  }
+void embed(arma::Mat<double>& source, 
+           mlpack::data::ImageInfo& sourceInfo, 
+           arma::Mat<double>& dest,
+           mlpack::data::ImageInfo& destInfo,
+           size_t dx,
+           size_t dy) {
 
-  Image letterboxImage(size_t newWidth, size_t newHeight) {
-    int newW = this->width;
-    int newH = this->height;
-    if (((float)newWidth/newW) < ((float)newHeight/newH)) {
-        newW = newWidth;
-        newH = this->height * newWidth / this->width;
-    } else {
-        newH = newHeight;
-        newW = this->width * newHeight / this->height;
-    }
-    Image resized;
-    resize(*this, resized, newW, newH);
-    Image boxed(newWidth, newHeight, this->channels);
-    boxed.fill(0.5);//fill with gray
-    embed(resized, boxed, (newWidth - newW)/2, (newHeight - newH)/2);
-    return boxed;
-  }
+  assert(sourceInfo.Channels() == destInfo.Channels());
+  size_t width = std::min(sourceInfo.Width() + dx, destInfo.Width());
+  size_t height = std::min(sourceInfo.Height() + dy, destInfo.Height());
 
-  void drawBox(size_t x, size_t y, size_t w, size_t h, size_t border) {
-    if (x < 0) x = 0;
-    if (x >= this->width) x = this->width - 1;
-
-    if (y < 0) y = 0;
-    if (y >= this->height) y = this->height - 1;
-
-    if (x+w > this->width) w = this->width - x;
-    if (x+w > this->width) w = this->width - x;
-
-    if (y+h > this->height) h = this->height - y;
-    if (y+h > this->height) h = this->height - y;
-
-    for (size_t b = 0; b < border; b++) {
-      for (size_t i = x; i < w; i++) {
-        for (size_t j = y; j < h; j++) {
-          setPixel(i, j, 0, 1.0f);//TODO:dont fill
-          setPixel(i, j, 1, 1.0f);
-          setPixel(i, j, 2, 1.0f);
-        }
+  for (size_t c = 0; c < sourceInfo.Channels(); c++) {
+    for (size_t i = 0; i < sourceInfo.Width(); i++) {
+      if (dx + i > destInfo.Width()) break;
+      for (size_t j = 0; j < sourceInfo.Height(); j++) {
+        if (dy + j > destInfo.Height()) break;
+        size_t sourceIndex = j*sourceInfo.Channels()*sourceInfo.Width() + i * sourceInfo.Channels() + c;
+        size_t destIndex = (j + dy)*destInfo.Channels()*destInfo.Width() + (i + dx) * destInfo.Channels() + c;
+        dest.at(destIndex) = source.at(sourceIndex);
       }
     }
   }
-
-  void setPixel(size_t x, size_t y, size_t c, double value) {
-    assert(x >= 0 && x < width);
-    assert(y >= 0 && y < width);
-    assert(c >= 0 && c < channels);
-
-    size_t index = c * width * height + y * width + x;
-    data[index] = value;
-  }
-
-  double getPixel(size_t x, size_t y, size_t c) {
-    assert(x >= 0 && x < width);
-    assert(y >= 0 && y < width);
-    assert(c >= 0 && c < channels);
-
-    size_t index = c * width * height + y * width + x;
-    return data[index];
-  }
-
-  void fill(double value) {
-    std::fill(data.begin(), data.end(), value);
-  }
-
-private:
-  size_t width, height, channels;
-  std::vector<double> data;
-};
+}
 
 int main(void) {
+  const std::string input = "input.jpg";
+  const std::string output = "output.jpg";
+
+  mlpack::data::ImageInfo inputInfo;
+  mlpack::data::ImageInfo outputInfo;
+
+  arma::mat inputData;
+  load(input, inputData, inputInfo);
+  save(output, inputData, inputInfo);
+
+  arma::mat outputData = resize(inputData, inputInfo, outputInfo, 2, 2);
+  //fill(outputData, 1);
+  //embed(inputData, inputInfo, outputData, outputInfo, 600, 801);
+
+  save(output, outputData, outputInfo);
+
   return 0;
 }
