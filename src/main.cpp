65d76b8e4c0cccec6ffbacb9491e50b1414b1a0c
Andrew Furey <andrewfurey2003@gmail.com>
2025-09-16 18:38:02 +0100

    citation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-// TODO: add citation to paper
+/*
+ *
+ * @article{DBLP:journals/corr/abs-1804-02767,
+ *   author       = {Joseph Redmon and Ali Farhadi},
+ *   title        = {YOLOv3: An Incremental Improvement},
+ *   journal      = {CoRR},
+ *   volume       = {abs/1804.02767},
+ *   year         = {2018},
+ *   url          = {http://arxiv.org/abs/1804.02767},
+ *   eprinttype    = {arXiv},
+ *   eprint       = {1804.02767},
+ *   timestamp    = {Mon, 13 Aug 2018 16:48:24 +0200},
+ *   biburl       = {https://dblp.org/rec/journals/corr/abs-1804-02767.bib},
+ *   bibsource    = {dblp computer science bibliography, https://dblp.org}
+ * }
+ *
+ */
 
 #include <mlpack.hpp>
 
