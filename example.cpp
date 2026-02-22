5f9e2716dc98bad1db395c9752706d95e18cf6a6
andrewfurey21 <andrewfurey2003@gmail.com>
2026-02-22 15:17:57 -0800

    add comment

--- example.cpp
+++ example.cpp
@@ -1,6 +1,7 @@
 #define MLPACK_ANN_IGNORE_SERIALIZATION_WARNING
 #include <mlpack.hpp>
 
+// Used for deserializing the model and loading the weights
 CEREAL_REGISTER_TYPE(mlpack::Layer<arma::fmat>)
 CEREAL_REGISTER_TYPE(mlpack::Identity<arma::fmat>)
 CEREAL_REGISTER_TYPE(mlpack::MultiLayer<arma::fmat>)
